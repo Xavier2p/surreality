4446fa69bb29893839b2913c72c4a92591db0865
Xavier2p <xavier.de-place@epita.fr>
2023-08-12 01:33:48 +0200

    Add deps and fmt

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Surreality from './app'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Surreality from './app';
+import './index.css';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Surreality />
-  </React.StrictMode>,
-)
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Surreality />
+    </MantineProvider>
+  </React.StrictMode>
+);
