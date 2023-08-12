52cbdcaa8420992123ed225307e0a6bc2cf4947b
Xavier2p <xavier.de-place@epita.fr>
2023-08-12 02:03:27 +0200

    Add deps and workflows

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//! Main entry point for the API server.
+#[macro_use]
+extern crate rocket;
+
+/// The root path of the API.
+#[get("/")]
+fn world() -> &'static str {
+    "hello, world"
+}
+
+/// The main entry point for the API server.
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build().mount("/", routes![world]).launch().await?;
+
+    Ok(())
 }
